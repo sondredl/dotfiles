87575a573603f87ba096c7be9fecb6f79da4a65e
Sondre Lindkjolen <sondre.lindkjolen@spacenorway.com>
2026-03-10 13:33:15 +0100

    disables explorer, finally

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
--- Disable netrw early
+-- Disable netrw *before anything else*
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- ~/.config/nvim/lua/config/options.lua  (or any early-loaded lua file)
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,20 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- Close any netrw/Explorer windows on startup (keep only neo-tree)
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-      local buf = vim.api.nvim_win_get_buf(win)
-      local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-      if ft == "netrw" or ft == "explorer" then
-        vim.api.nvim_win_close(win, true)
-      end
+-- Default autocmds: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+
+-- Helper: close ALL snaps_picker_list windows (the unwanted "Explorer")
+local function close_snaps_picker_windows()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+    if ft == "snaps_picker_list" then
+      vim.api.nvim_win_close(win, true)
     end
-  end,
+  end
+end
+
+-- 1. Close on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = close_snaps_picker_windows,
+})
+
+-- 2. Also close whenever any window is created
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinNew", "WinEnter" }, {
+  callback = close_snaps_picker_windows,
 })
